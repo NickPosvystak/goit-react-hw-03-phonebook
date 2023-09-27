aed67cd38c6e6ec00fbc71b4b68ec97442bd2d19
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-09-27 15:36:05 +0200

    add style input

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,17 +2,14 @@ import css from './Filter.module.css';
 
 const Filter = ({ filter, onFilterChange }) => {
   return (
-    <div className={css.inputFilter}>
-      {' '}
-      <input
-        className={css.inputFilter}
-        type="text"
-        name="filter"
-        placeholder="Search contacts"
-        value={filter}
-        onChange={e => onFilterChange(e.target.value)}
-      />
-    </div>
+    <input
+      className={css.inputFilter}
+      type="text"
+      name="filter"
+      placeholder="Search contacts"
+      value={filter}
+      onChange={e => onFilterChange(e.target.value)}
+    />
   );
 };
 export default Filter;
